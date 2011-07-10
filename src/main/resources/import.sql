d0613625c134a25e07d86f03e6a186edf24ab37c
Earle Nietzel <earle.nietzel@gmail.com>
2011-07-10 03:44:18 -0400

    Licensed under gplv3 using maven license plugin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,20 @@
+--
+-- Copyright (C) 2011 Earle Nietzel <earle.nietzel@gmail.com>
+--
+-- This file is part of SVFC57.
+--
+-- SVFC57 is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- SVFC57 is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with SVFC57.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 insert into USER (USERNAME, PASSWORD, ENABLED, ACCOUNTNONLOCKED, ACCOUNTNONEXPIRED, CREDENTIALSNONEXPIRED, AUTHORITY) values ('admin', '3f0149558bb3967a270a5014507f1911399b283f', true, true, true, true, 'ROLE_ADMIN');
